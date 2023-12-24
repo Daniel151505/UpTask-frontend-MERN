00464057012def096f708c7171fa3ebcfe48d9ab
Daniel151505 <daniel150520011@gmail.com>
2023-12-23 21:42:38 -0500

    feat: Validating the form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
-const [name, setName] = useState("");
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-const [repeatPassword, setRepeatPassword] = useState("");
+import Alert from "../components/Alert";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [alert, setAlert] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([name, email, password, repeatPassword].includes("")) {
+      setAlert({
+        msg: "All the fields are required",
+        error: true,
+      });
+      return;
+    }
+  };
+
+  const { msg } = alert;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
@@ -14,7 +30,12 @@ const Register = () => {
         <span className="text-slate-700">projects</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      {msg && <Alert alert={alert} />}
+
+      <form
+        className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             htmlFor="name"
