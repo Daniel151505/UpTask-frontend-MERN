0f1709a4796cd1be73be0966246d3c0e812d2f9b
Daniel151505 <daniel150520011@gmail.com>
2023-12-25 21:53:07 -0500

    feat: Validating the Token to Reset the Password

--- src/pages/NewPassword.jsx
+++ src/pages/NewPassword.jsx
@@ -1,4 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Alert from "../components/Alert";
+
 const NewPassword = () => {
+  const [validToken, setValidToken] = useState(false);
+  const [alert, setAlert] = useState({});
+
+  const params = useParams();
+  const { token } = params;
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        await axios.get(
+          `${
+            import.meta.env.VITE_BACKEND_URL
+          }/api/usuarios/olvide-password/${token}`
+        );
+
+        setValidToken(true);
+      } catch (error) {
+        setAlert({
+          msg: error.response.data.msg,
+          error: true,
+        });
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  const { msg } = alert;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
@@ -6,28 +40,32 @@ const NewPassword = () => {
         <span className="text-slate-700">projects</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
-        <div className="my-5">
-          <label
-            htmlFor="password"
-            className="uppercase text-gray-600 block text-xl font-bold"
-          >
-            New Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            placeholder="Enter your new password"
-            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-          />
-        </div>
+      {msg && <Alert alert={alert} />}
 
-        <input
-          type="submit"
-          value="Save New Password"
-          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
-        />
-      </form>
+      {validToken && (
+        <form className="my-10 bg-white shadow rounded-lg p-10">
+          <div className="my-5">
+            <label
+              htmlFor="password"
+              className="uppercase text-gray-600 block text-xl font-bold"
+            >
+              New Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Enter your new password"
+              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Save New Password"
+            className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+          />
+        </form>
+      )}
     </>
   );
 };
