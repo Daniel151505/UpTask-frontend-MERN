b2817664142ebfdc3fc50814155aced6496511a3
Daniel151505 <daniel150520011@gmail.com>
2023-12-25 22:41:11 -0500

    feat: Validated Authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,41 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Alert from "../components/Alert";
+import clientAxios from "../config/clientAxios";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alert, setAlert] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if ([email, password].includes("")) {
+      setAlert({
+        msg: "All the field are required",
+        error: true,
+      });
+      return;
+    }
+
+    
+  };
+
+  const { msg } = alert;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Login and manage your <span className="text-slate-700">projects</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      {msg && <Alert alert={alert} />}
+
+      <form
+        className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             htmlFor="email"
@@ -20,6 +48,8 @@ const Login = () => {
             type="email"
             placeholder="Enter your email"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -35,6 +65,8 @@ const Login = () => {
             type="password"
             placeholder="Enter your password"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
