3ae6bdfb4d1f24e6d240731d663236b6afb44a1d
Daniel151505 <daniel150520011@gmail.com>
2023-12-26 00:18:28 -0500

    feat: Checking the Token to Authenticate the user

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,9 +1,35 @@
 import { createContext, useEffect, useState } from "react";
+import clientAxios from "../config/clientAxios";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
+
+  useEffect(() => {
+    const authenticatedUser = async () => {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        return;
+      }
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      try {
+        const { data } = await clientAxios.get("/usuarios/perfil", config);
+        setAuth(data);
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    authenticatedUser();
+  }, []);
 
   return (
     <AuthContext.Provider
