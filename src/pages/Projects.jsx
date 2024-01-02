e01b63c9c1a07d036889eda56eadf3a30d7de813
Daniel151505 <daniel150520011@gmail.com>
2024-01-01 21:22:19 -0500

    feat: Obtaining User Projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,9 +5,17 @@ const Projects = () => {
 
   return (
     <>
-      <h1 className="teext-4xl font-black">Proyects</h1>
+      <h1 className="text-4xl font-black">Projects</h1>
 
-      <div></div>
+      <div className="bg-white shadow mt-10 rounded-lg p-5">
+        {projects.length ? (
+          <p>Yes, there are projects</p>
+        ) : (
+          <p className="text-center text-gray-600 uppercase">
+            No, there are still projects
+          </p>
+        )}
+      </div>
     </>
   );
 };
